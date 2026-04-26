b844d1687def7581b02b5256f73c2ce67beee4d7
Felipe <felipaladino05@gmail.com>
2026-04-26 12:56:35 -0300

    agrego codigo para esp ap, de momento muevo el sta a otro file (de momento)

--- Laboratorio_2b/main/Laboratorio_2b.c
+++ Laboratorio_2b/main/Laboratorio_2b.c
@@ -1,46 +1,35 @@
-#include "esp_err.h"
-#include "esp_event.h"
-#include "esp_log.h"
-#include "esp_netif.h"
-#include "esp_wifi.h"
-#include "nvs_flash.h"
+#define AP_SSID "ESP32_AP"
+#define AP_PASSWD "embedded"
 
-#define SSID "caliope"
-#define PASSWD "sinlugar"
-
-static void wifi_start(void);
-
-void app_main(void) {
-
-  esp_err_t ret = nvs_flash_init();
-  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-      ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-    ESP_ERROR_CHECK(nvs_flash_erase());
-    ret = nvs_flash_init();
-  }
-  ESP_ERROR_CHECK(ret);
-  wifi_start();
-}
-
-static void wifi_start(void) {
-  ESP_ERROR_CHECK(esp_netif_init());
+static void wifi_start_ap(void)
+{
+ ESP_ERROR_CHECK(esp_netif_init());
   ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-  esp_netif_create_default_wifi_sta();
+  esp_netif_create_default_wifi_ap();
 
-  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+  wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_netif_init(&config));
 
   wifi_config_t wifi_config = {
-      .sta =
-          {
-              .ssid = SSID,
-              .password = PASSWD,
-          },
+    .ap = {
+      .ssid = AP_SSID,
+      .ssid_len = strlen(AP_SSID),
+      .password = AP_PASSWD,
+      .max_connection = 4,
+      .authmode = WIFI_AUTH_WPA_WPA2_PSK,
+      .channel = 1
+    },
   };
 
-  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+  if (strlen(AP_PASS) == 0) {
+    wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+  }
+
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
   ESP_ERROR_CHECK(esp_wifi_start());
-  ESP_ERROR_CHECK(esp_wifi_connect());
+
+  ESP_LOGI("WIFI", "AP inicializado");
 }
+
