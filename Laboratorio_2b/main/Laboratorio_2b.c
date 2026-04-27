aed9e15fc2bf7b00f90f705dbef43d5476977edd
Felipe <felipaladino05@gmail.com>
2026-04-26 21:27:13 -0300

    Sono state testate tutte le possibili configurazioni dell'ESP32 come punto di accesso

--- Laboratorio_2b/main/Laboratorio_2b.c
+++ Laboratorio_2b/main/Laboratorio_2b.c
@@ -1,35 +1,61 @@
+#include <string.h>
+
+#include "esp_err.h"
+#include "esp_event.h"
+#include "esp_log.h"
+#include "esp_netif.h"
+#include "esp_wifi.h"
+#include "nvs_flash.h"
+
 #define AP_SSID "ESP32_AP"
-#define AP_PASSWD "embedded"
+#define AP_PASSWD "embedded" // dejar vacio para red abierta
+
+static void wifi_start_ap(void);
+
+void app_main(void)
+{
+    esp_err_t ret = nvs_flash_init();
+
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+        ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+
+    ESP_ERROR_CHECK(ret);
+
+    wifi_start_ap();
+}
 
 static void wifi_start_ap(void)
 {
- ESP_ERROR_CHECK(esp_netif_init());
-  ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-  esp_netif_create_default_wifi_ap();
+    esp_netif_create_default_wifi_ap();
 
-  wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
-  ESP_ERROR_CHECK(esp_netif_init(&config));
+    wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&config));
 
-  wifi_config_t wifi_config = {
+    wifi_config_t wifi_config = {
     .ap = {
-      .ssid = AP_SSID,
-      .ssid_len = strlen(AP_SSID),
-      .password = AP_PASSWD,
-      .max_connection = 4,
-      .authmode = WIFI_AUTH_WPA_WPA2_PSK,
-      .channel = 1
+        .ssid = AP_SSID,
+        .ssid_len = strlen(AP_SSID),
+        .password = AP_PASSWD,
+        .max_connection = 1,
+        .authmode = WIFI_AUTH_WPA_WPA2_PSK,
+        .channel = 1,
+        // .ssid_hidden = 1,
     },
-  };
+};
+    // Si la password es vacia, entonces signfica que la red es abierta y que funciona igual
+    if (strlen(AP_PASSWD) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
 
-  if (strlen(AP_PASS) == 0) {
-    wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-  }
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
 
-  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
-  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
-  ESP_ERROR_CHECK(esp_wifi_start());
-
-  ESP_LOGI("WIFI", "AP inicializado");
+    ESP_LOGI("WIFI", "AP inicializado. SSID: %s", AP_SSID);
 }
-
