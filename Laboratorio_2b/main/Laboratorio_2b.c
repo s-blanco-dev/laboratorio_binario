b98b03503081e51fad3c7193998fb0f186908124
Felipe <felipaladino05@gmail.com>
2026-04-28 12:09:55 -0300

    両方の書類を結合します。

--- Laboratorio_2b/main/Laboratorio_2b.c
+++ Laboratorio_2b/main/Laboratorio_2b.c
@@ -10,7 +10,11 @@
 #define AP_SSID "ESP32_AP"
 #define AP_PASSWD "embedded" // dejar vacio para red abierta
 
+#define SSID "caliope"
+#define PASSWD ""
+
 static void wifi_start_ap(void);
+static void wifi_start_sta(void);
 
 void app_main(void)
 {
@@ -25,6 +29,7 @@ void app_main(void)
     ESP_ERROR_CHECK(ret);
 
     wifi_start_ap();
+    // wifi_start_sta();
 }
 
 static void wifi_start_ap(void)
@@ -59,3 +64,30 @@ static void wifi_start_ap(void)
 
     ESP_LOGI("WIFI", "AP inicializado. SSID: %s", AP_SSID);
 }
+
+// == Parte 3 == 
+
+static void wifi_start_sta(void) {
+  ESP_ERROR_CHECK(esp_netif_init());
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+
+  esp_netif_create_default_wifi_sta();
+
+  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  wifi_config_t wifi_config = {
+      .sta =
+          {
+              .ssid = SSID,
+              .password = PASSWD,
+          },
+  };
+
+  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+  ESP_ERROR_CHECK(esp_wifi_start());
+  ESP_ERROR_CHECK(esp_wifi_connect());
+}
+
+
