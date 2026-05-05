54a3ced6b49e6563a2c4c63f07ead0a7cda002b6
s-blanco-dev <santiago.blancoc@correo.ucu.edu.uy>
2026-05-05 17:58:04 -0300

    tuberculosis pulmonar

--- Laboratorio_2b/main/Laboratorio_2b.c
+++ Laboratorio_2b/main/Laboratorio_2b.c
@@ -5,6 +5,7 @@
 #include "esp_http_server.h"
 #include "esp_log.h"
 #include "esp_netif.h"
+#include "esp_netif_types.h"
 #include "esp_wifi.h"
 #include "esp_wifi_types_generic.h"
 #include "nvs_flash.h"
@@ -18,13 +19,13 @@
 #define MAX_RETRY 5
 
 extern const uint8_t index_html_start[] asm("_binary_index_html_start");
-extern const uint8_t index_html_end[]   asm("_binary_index_html_end");
+extern const uint8_t index_html_end[] asm("_binary_index_html_end");
 
 extern const uint8_t style_css_start[] asm("_binary_style_css_start");
-extern const uint8_t style_css_end[]   asm("_binary_style_css_end");
+extern const uint8_t style_css_end[] asm("_binary_style_css_end");
 
 extern const uint8_t app_js_start[] asm("_binary_app_js_start");
-extern const uint8_t app_js_end[]   asm("_binary_app_js_end");
+extern const uint8_t app_js_end[] asm("_binary_app_js_end");
 
 static void wifi_start_ap(void);
 static void wifi_start_sta(void);
@@ -33,19 +34,37 @@ static const char *TAG = "alfredo arno bot";
 static int s_retry_num = 0;
 
 void app_main(void) {
-  esp_err_t ret = nvs_flash_init();
+    esp_err_t ret = nvs_flash_init();
 
-  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
-      ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-    ESP_ERROR_CHECK(nvs_flash_erase());
-    ret = nvs_flash_init();
-  }
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
 
-  ESP_ERROR_CHECK(ret);
+    ESP_ERROR_CHECK(ret);
 
-  // wifi_start_ap();
-  wifi_start_sta();
-  // start_webserver();
+    wifi_start_ap();
+    // wifi_start_sta();
+    start_webserver();
+}
+
+static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
+    if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
+        esp_wifi_connect();
+    } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+        if (s_retry_num < MAX_RETRY) {
+            esp_wifi_connect();
+            s_retry_num++;
+            ESP_LOGI(TAG, "retry to connect to the AP");
+        } else {
+            wifi_event_sta_disconnected_t *event = (wifi_event_sta_disconnected_t *)event_data;
+            ESP_LOGE("WIFI_DEBUG", "Falleció por razón: %d", event->reason);
+        }
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
+        ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
+        s_retry_num = 0;
+    }
 }
 
 /**
@@ -70,55 +89,43 @@ void app_main(void) {
  * @return No retorna.
  */
 static void wifi_start_ap(void) {
-  ESP_ERROR_CHECK(esp_netif_init());
-  ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-  esp_netif_create_default_wifi_ap();
+    esp_netif_create_default_wifi_ap();
 
-  wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
-  ESP_ERROR_CHECK(esp_wifi_init(&config));
+    wifi_init_config_t config = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&config));
 
-  wifi_config_t wifi_config = {
-      .ap =
-          {
-              .ssid = AP_SSID,
-              .ssid_len = strlen(AP_SSID),
-              .password = AP_PASSWD,
-              .max_connection = 1,
-              .authmode = WIFI_AUTH_WPA_WPA2_PSK,
-              .channel = 1,
-              // .ssid_hidden = 1,
-          },
-  };
-  // Si la password es vacia, entonces signfica que la red es abierta y que
-  // funciona igual
-  if (strlen(AP_PASSWD) == 0) {
-    wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-    // wifi_start_ap();
-    wifi_start_sta();
-  }
-}
-
-static void event_handler(void *arg, esp_event_base_t event_base,
-                          int32_t event_id, void *event_data) {
-  if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
-    esp_wifi_connect();
-  } else if (event_base == WIFI_EVENT &&
-             event_id == WIFI_EVENT_STA_DISCONNECTED) {
-    if (s_retry_num < MAX_RETRY) {
-      esp_wifi_connect();
-      s_retry_num++;
-      ESP_LOGI(TAG, "retry to connect to the AP");
-    } else {
-      wifi_event_sta_disconnected_t *event =
-          (wifi_event_sta_disconnected_t *)event_data;
-      ESP_LOGE("WIFI_DEBUG", "Falleció por razón: %d", event->reason);
+    wifi_config_t wifi_config = {
+        .ap =
+            {
+                .ssid = AP_SSID,
+                .ssid_len = strlen(AP_SSID),
+                .password = AP_PASSWD,
+                .max_connection = 1,
+                .authmode = WIFI_AUTH_WPA_WPA2_PSK,
+                .channel = 1,
+                // .ssid_hidden = 1,
+            },
+    };
+    // Si la password es vacia, entonces signfica que la red es abierta y que
+    // funciona igual
+    if (strlen(AP_PASSWD) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
     }
-  } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
-    ESP_LOGI(TAG, "got ip:" IPSTR, IP2STR(&event->ip_info.ip));
-    s_retry_num = 0;
-  }
+
+
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_AP));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_AP, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(
+        esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
+    ESP_ERROR_CHECK(
+        esp_event_handler_instance_register(IP_EVENT, IP_EVENT_AP_STAIPASSIGNED, &event_handler, NULL, &instance_got_ip));
 }
 
 // == Ejercicio 3 ===
@@ -139,10 +146,10 @@ static void event_handler(void *arg, esp_event_base_t event_base,
  * @return ESP_OK si la respuesta fue enviada correctamente.
  */
 static esp_err_t root_get_handler(httpd_req_t *req) {
-  const char *response = "Ciao Mondo";
-  // el hanlder responde a la preticion del navegador con el texto de arriba
-  httpd_resp_send(req, response, HTTPD_RESP_USE_STRLEN);
-  return ESP_OK;
+    const char *response = "Ciao Mondo";
+    // el hanlder responde a la preticion del navegador con el texto de arriba
+    httpd_resp_send(req, response, HTTPD_RESP_USE_STRLEN);
+    return ESP_OK;
 }
 
 /**
@@ -178,17 +185,17 @@ static const httpd_uri_t root_uri = {
  * pudo iniciarse.
  */
 static httpd_handle_t start_webserver(void) {
-  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
+    httpd_config_t config = HTTPD_DEFAULT_CONFIG();
 
-  httpd_handle_t server = NULL;
+    httpd_handle_t server = NULL;
 
-  if (httpd_start(&server, &config) == ESP_OK) {
-    httpd_register_uri_handler(server, &root_uri);
-    ESP_LOGI("WEB", "Servidor HTTP iniciado");
-  } else {
-    ESP_LOGE("WEB", "Error capital al iniciar el servidor HTTP");
-  }
-  return server;
+    if (httpd_start(&server, &config) == ESP_OK) {
+        httpd_register_uri_handler(server, &root_uri);
+        ESP_LOGI("WEB", "Servidor HTTP iniciado");
+    } else {
+        ESP_LOGE("WEB", "Error capital al iniciar el servidor HTTP");
+    }
+    return server;
 }
 
 /*
@@ -202,35 +209,36 @@ static httpd_handle_t start_webserver(void) {
  *
  * */
 static void wifi_start_sta(void) {
-  ESP_ERROR_CHECK(esp_netif_init());
-  ESP_ERROR_CHECK(esp_event_loop_create_default());
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
 
-  esp_netif_create_default_wifi_sta();
+    esp_netif_create_default_wifi_sta();
 
-  wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
-  ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+    wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&cfg));
 
-  wifi_config_t wifi_config = {
-      .sta =
-          {
-              .ssid = SSID, .password = PASSWD,
-          },
+    wifi_config_t wifi_config = {
+        .sta =
+            {
+                .ssid = SSID,
+                .password = PASSWD,
+            },
 
-  };
+    };
 
-  if (strlen(AP_PASSWD) == 0) {
-    wifi_config.ap.authmode = WIFI_AUTH_OPEN;
-  }
+    if (strlen(AP_PASSWD) == 0) {
+        wifi_config.ap.authmode = WIFI_AUTH_OPEN;
+    }
 
-  ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-  ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
-  ESP_ERROR_CHECK(esp_wifi_start());
-  ESP_ERROR_CHECK(esp_wifi_connect());
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(WIFI_IF_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_connect());
 
-  esp_event_handler_instance_t instance_any_id;
-  esp_event_handler_instance_t instance_got_ip;
-  ESP_ERROR_CHECK(esp_event_handler_instance_register(
-      WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
-  ESP_ERROR_CHECK(esp_event_handler_instance_register(
-      IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
+    esp_event_handler_instance_t instance_any_id;
+    esp_event_handler_instance_t instance_got_ip;
+    ESP_ERROR_CHECK(
+        esp_event_handler_instance_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
+    ESP_ERROR_CHECK(
+        esp_event_handler_instance_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
 }
