622b2770a9ccb456fa2edab4c2dfbe79743b7ade
s-blanco-dev <santiago.blancoc@correo.ucu.edu.uy>
2026-05-07 19:13:20 -0300

    fracaso

---
+++ Laboratorio_2b/components/touchpad/touchpad.c
@@ -0,0 +1,112 @@
+#include "touchpad.h"
+#include "driver/touch_pad.h"
+#include "esp_log.h"
+
+#define TOUCH_THRESHOLD_COEFF 0.10f
+
+static const char *TAG = "touchpad";
+
+static const touch_pad_t s_pads[TOUCHPAD_NUM_BUTTONS] = {
+    TOUCHPAD_BTN_PHOTO_NUM,   // index 0 - PHOTO
+    TOUCHPAD_BTN_PLAY_NUM,    // index 1 - PLAY
+    TOUCHPAD_BTN_RECORD_NUM,  // index 2 - RECORD
+    TOUCHPAD_BTN_NETWORK_NUM, // index 3 - NETWORK no anda la puta que lo pario
+    TOUCHPAD_BTN_VOLUP_NUM,   // index 4 - VOL_UP
+    TOUCHPAD_BTN_VOLDOWN_NUM, // index 5 - VOL_DOWN
+    TOUCHPAD_BTN_GUARD_NUM,   // index 6 - GUARD
+};
+static uint32_t s_baseline[TOUCHPAD_NUM_BUTTONS];
+static uint32_t s_threshold[TOUCHPAD_NUM_BUTTONS];
+
+void touchpad_init(void) {
+    ESP_ERROR_CHECK(touch_pad_init());
+    ESP_ERROR_CHECK(touch_pad_set_fsm_mode(TOUCH_FSM_MODE_TIMER));
+
+    /* configure */
+    for (int i = 0; i < TOUCHPAD_NUM_BUTTONS; i++) {
+        ESP_LOGI(TAG, "Configuring pad %d with touch number %d", i, s_pads[i]);
+        ESP_ERROR_CHECK(touch_pad_config(s_pads[i]));
+    }
+    ESP_ERROR_CHECK(touch_pad_config(TOUCHPAD_BTN_GUARD_NUM));
+
+    /* denoise */
+    touch_pad_denoise_t denoise = {
+        .grade = TOUCH_PAD_DENOISE_BIT4,
+        .cap_level = TOUCH_PAD_DENOISE_CAP_L4,
+    };
+    ESP_ERROR_CHECK(touch_pad_denoise_set_config(&denoise));
+    ESP_ERROR_CHECK(touch_pad_denoise_enable());
+
+    /* Por las dudas I: no me la juego a probarlo */
+    touch_pad_waterproof_t waterproof = {
+        .guard_ring_pad = TOUCHPAD_BTN_GUARD_NUM, .shield_driver = TOUCH_PAD_SHIELD_DRV_L0, /*!< 40pf */
+    };
+    ESP_ERROR_CHECK(touch_pad_waterproof_set_config(&waterproof));
+    ESP_ERROR_CHECK(touch_pad_waterproof_enable());
+
+    /* Por las dudas II: IIR-16 filter + IIR-2 smoother */
+    touch_filter_config_t filter = {
+        .mode = TOUCH_PAD_FILTER_IIR_16,
+        .debounce_cnt = 1,
+        .noise_thr = 0,
+        .jitter_step = 4,
+        .smh_lvl = TOUCH_PAD_SMOOTH_IIR_2,
+    };
+    ESP_ERROR_CHECK(touch_pad_filter_set_config(&filter));
+    ESP_ERROR_CHECK(touch_pad_filter_enable());
+
+    ESP_ERROR_CHECK(touch_pad_fsm_start());
+
+    /* mmmmmm */
+    esp_rom_delay_us(200000);
+
+    /* calibra */
+    for (int i = 0; i < TOUCHPAD_NUM_BUTTONS; i++) {
+        ESP_ERROR_CHECK(touch_pad_read_benchmark(s_pads[i], &s_baseline[i]));
+        s_threshold[i] = (uint32_t)((float)s_baseline[i] * TOUCH_THRESHOLD_COEFF);
+        ESP_ERROR_CHECK(touch_pad_set_thresh(s_pads[i], s_threshold[i]));
+        ESP_LOGI(TAG, "Pad[%d] touch_num=%d baseline=%" PRIu32 " threshold=%" PRIu32, i, s_pads[i], s_baseline[i],
+                 s_threshold[i]);
+    }
+}
+
+bool touchpad_is_pressed(uint8_t btn_index) {
+    if (btn_index >= TOUCHPAD_NUM_BUTTONS) {
+        return false;
+    }
+
+    // /* skip cursed button */
+    // if (btn_index == TOUCHPAD_BTN_NETWORK) {
+    //     return false;
+    // }
+
+    uint32_t raw = 0;
+    ESP_ERROR_CHECK(touch_pad_read_raw_data(s_pads[btn_index], &raw));
+
+    if (raw <= s_baseline[btn_index]) {
+        return false;
+    }
+    return (raw - s_baseline[btn_index]) > s_threshold[btn_index];
+}
+
+#include <inttypes.h>
+
+void touchpad_debug_print(void) {
+    for (int i = 0; i < TOUCHPAD_NUM_BUTTONS; i++) {
+        uint32_t raw = 0;
+        touch_pad_read_raw_data(s_pads[i], &raw);
+
+        uint32_t diff = 0;
+        bool pressed = false;
+
+        // aumenta los valores
+        if (raw > s_baseline[i]) {
+            diff = raw - s_baseline[i];
+            pressed = diff > s_threshold[i];
+        }
+
+        printf("Button %d (TOUCH%d): raw=%5" PRIu32 " base=%5" PRIu32 " diff=%4" PRIu32 " -> %s\n", i, s_pads[i], raw,
+               s_baseline[i], diff, pressed ? "TOUCHED!" : "---");
+    }
+    printf("========================================\n");
+}
--- Laboratorio_2b/main/Laboratorio_2b.c
+++ Laboratorio_2b/main/Laboratorio_2b.c
@@ -9,6 +9,7 @@
 #include "esp_netif.h"
 #include "esp_wifi.h"
 #include "nvs_flash.h"
+#include "web_utils.h"
 // #include "rgb_led.h" // novedad
 
 #define AP_SSID "ESP32_AP"
@@ -19,20 +20,10 @@
 
 #define MAX_RETRY 5
 
-extern const uint8_t index_html_start[] asm("_binary_index_html_start");
-extern const uint8_t index_html_end[] asm("_binary_index_html_end");
-
-extern const uint8_t style_css_start[] asm("_binary_style_css_start");
-extern const uint8_t style_css_end[] asm("_binary_style_css_end");
-
-extern const uint8_t app_js_start[] asm("_binary_app_js_start");
-extern const uint8_t app_js_end[] asm("_binary_app_js_end");
-
 /* ======== */
 
 static void wifi_start_ap(void);
 static void wifi_start_sta(void);
-static httpd_handle_t start_webserver(void);
 static const char *TAG = "alfredo arno bot";
 static int s_retry_num = 0;
 
@@ -128,136 +119,6 @@ static void event_handler(void *arg, esp_event_base_t event_base,
   }
 }
 
-// == Ejercicio 3 ===
-/**
- * @brief Atiende las peticiones HTTP GET realizadas a la ruta raíz del
- * servidor.
- *
- * Esta función actúa como handler de la URI "/". Se ejecuta automáticamente
- * cuando un navegador o cliente HTTP accede a la dirección principal del
- * ESP32, por ejemplo: http://192.168.4.1/.
- *
- * En esta primera implementación responde con un texto simple, permitiendo
- * verificar que el servidor HTTP embebido está funcionando correctamente.
- *
- * @param req Puntero a la estructura que representa la petición HTTP
- * recibida.
- *
- * @return ESP_OK si la respuesta fue enviada correctamente.
- */
-static esp_err_t root_get_handler(httpd_req_t *req) {
-  size_t index_html_size = index_html_end - index_html_start;
-  httpd_resp_send(req, (const char *)index_html_start, index_html_size);
-  return ESP_OK;
-}
-
-/**
- * @brief Define la URI raiz del servidor HTTP.
- *
- * Asocia la ruta "/" con el metodo HTTP GET y con la fucnion
- * root_get_handler(). Cuando un cliente entra a la pagina principal del
- * ESP32, el servidor ejecuta este handler para generar la respuesta.
- */
-static const httpd_uri_t root_uri = {
-    .uri = "/",
-    .method = HTTP_GET,
-    .handler = root_get_handler,
-    .user_ctx = NULL, // *Abdul Bari reference
-};
-
-typedef struct {
-  int r;
-  int g;
-  int b;
-} led_state_t;
-
-// la idea por ahora es, cada vez que se actualice el led en la web, se
-// actualice current_led
-static led_state_t current_led = {.r = 0, .g = 0, .b = 0};
-
-/**
- * @brief Atiende las peticiones HTTP POST enviadas a la ruta "/led".
- *
- * Esta función se ejecuta cuando la página web envía un comando al ESP32
- * para cambiar el color del LED RGB. El navegador manda en el cuerpo de la
- * petición un objeto JSON con los valores de rojo, verde y azul, por ejemplo:
- *
- * { "r": 255, "g": 0, "b": 128 }
- *
- * El handler lee el cuerpo de la petición HTTP, lo interpreta usando cJSON,
- * extrae los valores numéricos de los campos "r", "g" y "b", actualiza la
- * variable global que guarda el estado actual del LED y luego llama a la
- * función correspondiente de la librería rgb_led para aplicar físicamente
- * ese color en la placa.
- *
- * Si el JSON recibido es inválido o faltan campos, responde con un error HTTP.
- * Si todo sale correctamente, responde al navegador con un JSON simple
- * indicando que la operación fue realizada.
- *
- * @param req Puntero a la estructura que representa la petición HTTP recibida.
- *
- * @return ESP_OK si el color fue recibido y procesado correctamente.
- *         ESP_FAIL si ocurrió un error al recibir o parsear el JSON.
- */
-static esp_err_t led_post_handler(httpd_req_t *req) {
-  char buffer[128];
-
-  int received = httpd_req_recv(req, buffer, sizeof(buffer) - 1);
-
-  if (received <= 0) {
-    httpd_resp_send_404(req);
-    return ESP_FAIL;
-  }
-
-  buffer[received] = '\0';
-
-  cJSON *json = cJSON_Parse(buffer);
-
-  if (json == NULL) {
-    httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "JSON invalido");
-
-    cJSON *r_item = cJSON_GetObjectItem(json, "r");
-    cJSON *g_item = cJSON_GetObjectItem(json, "g");
-    cJSON *b_item = cJSON_GetObjectItem(json, "b");
-
-    if (!cJSON_IsNumber(r_item) || !cJSON_IsNumber(g_item) ||
-        !cJSON_IsNumber(b_item)) {
-      cJSON_Delete(json);
-      httpd_resp_send_err(req, HTTPD_400_BAD_REQUEST, "Faltan valores RGB");
-      return ESP_FAIL;
-    }
-
-    int r = r_item->valueint;
-    int g = g_item->valueint;
-    int b = b_item->valueint;
-
-    current_led.r = r;
-    current_led.g = g;
-    current_led.b = b;
-
-    /*
-     * Aca se debe llamar a la funcion real de la libreria del led
-     */
-
-    ESP_LOGI(TAG, "LED actualizado: R=%d, G=%d, B=%d", r, g, b);
-
-    cJSON_Delete(json);
-
-    httpd_resp_set_type(req, "application/json");
-    httpd_resp_send(req, "{\"status\":\"ok\"}", HTTPD_RESP_USE_STRLEN);
-
-    return ESP_OK;
-  }
-  return ESP_OK;
-}
-// uri para POST / led
-static const httpd_uri_t led_post_uri = {
-    .uri = "/led",
-    .method = HTTP_POST,
-    .handler = led_post_handler,
-    .user_ctx = NULL,
-};
-
 /* ======== */
 
 static void wifi_start_sta(void) {
@@ -288,38 +149,4 @@ static void wifi_start_sta(void) {
       WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL, &instance_any_id));
   ESP_ERROR_CHECK(esp_event_handler_instance_register(
       IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL, &instance_got_ip));
-}
-
-/**
- * @brief Inicializa y arranca el servidor HTTP embebido del ESP32.
- *
- * Esta función crea una configuración por defecto para el servidor HTTP
- * usando HTTPD_DEFAULT_CONFIG(), inicia el servidor mediante httpd_start() y
- * registra los handlers de las rutas que el servidor debe atender.
- *
- * En esta etapa del laboratorio se registra únicamente la ruta raíz "/",
- * asociada al método HTTP GET. Cuando un navegador accede a dicha ruta, se
- * ejecuta root_get_handler(), que devuelve una respuesta simple de prueba.
- *
- * El servidor se levanta después de inicializar el WiFi en modo AP, de forma
- * que un dispositivo conectado a la red generada por el ESP32 pueda acceder
- * desde un navegador a la dirección IP del punto de acceso.
- *
- * @return Handle del servidor HTTP si se inició correctamente, o NULL si no
- * pudo iniciarse. */
-static httpd_handle_t start_webserver(void) {
-  httpd_config_t config = HTTPD_DEFAULT_CONFIG();
-
-  httpd_handle_t server = NULL;
-
-  if (httpd_start(&server, &config) == ESP_OK) {
-    httpd_register_uri_handler(server, &root_uri);
-    // agrego la uri para POST / led
-    httpd_register_uri_handler(server, &led_post_uri);
-
-    ESP_LOGI("WEB", "Servidor HTTP iniciado");
-  } else {
-    ESP_LOGE("WEB", "Error capital al iniciar el servidor HTTP");
-  }
-  return server;
 }
